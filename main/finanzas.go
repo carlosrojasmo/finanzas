65a970a77db043b1029993b3823d8ef2ed039140
Carlos <carlos.rojasmo@sansano.usm.com>
2020-10-18 23:06:03 -0300

    conexion

--- main/finanzas.go
+++ main/finanzas.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+	"encoding/json"
 )
 
 var registerPedido= []pedido{}
 var registerGanancia=[]int{}
 
-type pedido struct{
+
+type paquete struct {
 	idPaquete string
-	intentos int
-	entregado bool
 	tipo string
 	valor int
+	seguimiento int
+	intentos int
+	estado string
+}
+type pedido struct{
+	idPaquete string
+	tipo string
+	valor int
+	intentos int
+	entregado bool
+	
+	
 }
 func sumSlices(x[]int) int {	
 
@@ -22,7 +36,7 @@ func sumSlices(x[]int) int {
 	}
 	return totalx
 }
-func newPedido(idPaquete string, intentos int, estregado bool,tipo string,valor int) pedido{
+func newPedido(idPaquete string, intentos int, entregado bool,tipo string,valor int) pedido{
 	pedidoNuevo := pedido{idPaquete:idPaquete,intentos:intentos,entregado: entregado, tipo:tipo,valor:valor}
 	return pedidoNuevo
 }
@@ -35,25 +49,74 @@ func calcularGanancia(pedido pedido) int{
 	}
 	if pedido.entregado==true{
 		ganancia=ganancia+pedido.valor
-		return (ganancia)
 	}else {
 		if pedido.tipo=="normal"{
-			return ganancia
 		}else if pedido.tipo=="retail"{
 			ganancia=ganancia+pedido.valor
-		}else{
-			return ganancia 
 		}
-	}	
+	}
+	return ganancia	
 }
+func failOnError(err error, msg string) {
+	if err != nil {
+	  log.Fatalf("%s: %s", msg, err)
+	}
+  }
 
 func main(){
 	//escucchar
 	//recibir 
-	nuevoPedido := newPedido(id,intentos,entragado,tipo,valor)
-	registerPedido=append(registerPedido,nuevoPedido)
-	ganancia:=calcularGanancia(nuevoPedido)
-	registerGanancia=append(registerGanancia,ganancia)
-	sum:=sumSlices(registerGanancia)
-	fmt.Println("la ganancia es: ",sum )
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+	    "hello", // name
+  		false,   // durable
+    	false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	  )
+	  failOnError(err, "Failed to register a consumer")
+	  
+	  forever := make(chan bool)
+	  
+	  go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			var paqueteCalcular paquete
+			var entregado bool
+			err=json.Unmarshal(d.Body,&paqueteCalcular)
+			if paqueteCalcular.estado=="Recibido"{
+				entregado=true
+			}else{
+				entregado=false
+			}
+			nuevoPedido := newPedido(paqueteCalcular.idPaquete,paqueteCalcular.intentos,entregado,paqueteCalcular.tipo,paqueteCalcular.valor)
+			registerPedido=append(registerPedido,nuevoPedido)
+			ganancia:=calcularGanancia(nuevoPedido)
+			registerGanancia=append(registerGanancia,ganancia)
+			sum:=sumSlices(registerGanancia)
+			fmt.Println("la ganancia es: ",sum )
+		}
+	  }()
+	  
+	  log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	  <-forever
+
 }
